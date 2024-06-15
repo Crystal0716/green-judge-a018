409343f463476a9ead13ebfd1ef492602d6599f4
若水 <168269104+Crystal0716@users.noreply.github.com>
2024-06-15 20:23:14 +0800

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,27 @@
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n,m;
+    cin>>n>>m;
+    if(n>=14&&n<=16)
+        {if(n=14&&m>=20)
+        {
+            cout<<"YES"<<endl;
+        }
+        else if (n=16&&m<=40)
+        {
+           cout<<"YES" <<endl;
+        }
+        else
+            cout<<"YES"<<endl;
+        }
+    else
+     {
+         cout<<"NO"<<endl;
+     }
+    return 0;
+}
